f35abb7bbc0b5333dac52692661ad9e6c2b4eebc
Leksaa3 <aleksa.kijaciccc@gmail.com>
2024-09-01 08:47:34 +0200

    Update config.js
    Signed-off-by: Leksaa3 <aleksa.kijaciccc@gmail.com>

--- other/config.js
+++ other/config.js
@@ -2,13 +2,13 @@ const cf = {
   config: {
 
 
-    guild_x: "None", //your guild id
+    guild_x: "1279104786855100447", //your guild id
 
-    role_x: "None", //slot user role id, you need this role in your profile to use slot bot 
+    role_x: "1279105498880413758", //slot user role id, you need this role in your profile to use slot bot 
 
-    staff_x: "None", // staff role id
+    staff_x: "1279104893474308178", // staff role id
 
-    logchannel: '', //channel id for slot revoke notice
+    logchannel: '1279693838990049411', //channel id for slot revoke notice
 
     slotrule: `set your slot rules`, // your slot rules
   }
